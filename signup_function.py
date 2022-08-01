20113f302fdb7bf0d61da1d1e2dbd37bd20b67f6
Mehedy Hassan Mukul <57004294+mukulmh@users.noreply.github.com>
2022-08-01 12:57:35 +0600

    signup function for new user

--- signup_function.py
+++ signup_function.py
@@ -1,19 +1,23 @@
-from unicodedata import name
+# Mehedy Hassan Mukul
+
 import mysql.connector
 import json
 
 def lambda_handler(event, context):
+
+    # Establish the connection
     try:
         connection = mysql.connector.connect(host='remotemysql.com',
                                             database='0SvQ34JYQz',
                                             user='0SvQ34JYQz',
                                             password='P1GVwRXVKP')
-                                            
-        body = json.loads(event['body'])
+    except:
+        result = {
+            "message": "Connection Error!"
+        }
 
-        inp_name = body['name']
-        inp_email = body['email']
-        inp_password = body['password']
+    # register a new user
+    def reg_user(inp_name, inp_email, inp_password):
 
         sql_select_Query = "select * from user where email = %s"
         mycursor = connection.cursor()
@@ -22,24 +26,37 @@ def lambda_handler(event, context):
         records = mycursor.fetchone()
         
         if not records:
-        
             try:
                 mycursor.execute('insert into user (name, email, password) values(%s, %s, %s)', (inp_name,inp_email,inp_password,))
                 connection.commit()
-                message = "Registration Success"
+                message ={
+                    "message": "Registration Success. Now log in with your email and password."
+                }
             except:
-                message = "Soemthing Went Wrong"
+                message ={
+                    "message": "Couldn't register, Please try again!"
+                }
         else:
-            message = "Email already taken!"
+            message ={
+                    "message": "User already exists!"
+                }
+
+        return message
+        
+    try:
+        body = json.loads(event['body'])
+        inp_name = body['name']
+        inp_email = body['email']
+        inp_password = body['password']
+
+        result = reg_user(inp_name,inp_email,inp_password)
 
     except:
-        message = "Something went wrong!"
-
-    data = {
-            "message" : message
+        result = {
+            "message": "Please provide name, email & password correctly!"
         }
-
+    
     return{
         'statusCode': 200,
-        'body': json.dumps(data)
+        'body': json.dumps(result)
     }
